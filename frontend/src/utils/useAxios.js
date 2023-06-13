ff462f41d50ecbebe7519b116da96164dcce098f
FTyumin <feodor.tjumin28@gmail.com>
2023-06-13 09:58:37 +0300

    Added views, trying to fix error

--- frontend/src/utils/useAxios.js
+++ frontend/src/utils/useAxios.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from './axios';
 import { getRefreshToken, isAccessTokenExpired, setAuthUser } from './auth';
 import { API_BASE_URL } from './constants';
 import Cookies from 'js-cookie';
