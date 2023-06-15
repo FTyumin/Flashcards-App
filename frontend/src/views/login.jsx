0c5bb9d143a4c8b4e18e802adef19e73a9a84586
FTyumin <feodor.tjumin28@gmail.com>
2023-06-15 16:06:03 +0300

    Edits

--- frontend/src/views/login.jsx
+++ frontend/src/views/login.jsx
@@ -3,6 +3,19 @@ import { login } from '../utils/auth';
 import { useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../store/auth';
 
+import {
+    TextInput,
+    PasswordInput,
+    Checkbox,
+    Anchor,
+    Paper,
+    Title,
+    Text,
+    Container,
+    Group,
+    Button,
+  } from "@mantine/core";
+
 const Login = () => {
     const navigate = useNavigate();
     const [username, setUsername] = useState('');
@@ -31,32 +44,43 @@ const Login = () => {
         }
     };
     return (
-        <section>
-            <h1>Login</h1>
-            <form onSubmit={handleLogin}>
-                <div>
-                    <label htmlFor="username">Username</label>
-                    <input
-                        type="text"
-                        id="username"
-                        name="username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <button type="submit">Login</button>
-            </form>
-        </section>
+        // <section>
+        //     <h1>Login</h1>
+        //     <Form onSubmit={handleLogin}>
+        //         <div>
+        //             <label htmlFor="username">Username</label>
+        //             <input
+        //                 type="text"
+        //                 id="username"
+        //                 name="username"
+        //                 value={username}
+        //                 onChange={(e) => setUsername(e.target.value)}
+        //             />
+        //         </div>
+        //         <div>
+        //             <label htmlFor="password">Password</label>
+        //             <input
+        //                 type="password"
+        //                 id="password"
+        //                 name="password"
+        //                 value={password}
+        //                 onChange={(e) => setPassword(e.target.value)}
+        //             />
+        //         </div>
+        //         <Button type="submit" variant="primary">Login</Button>
+        //     </Form>
+        // </section>
+        <Container size={420} my={40}>
+            <Title align="center" sx={(theme) =>({
+                fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+                fontWeight: 900,
+            })} 
+            >
+                Whatssup
+            </Title>
+        </Container>
+
+     
     );
 };
 
