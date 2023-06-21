ffdc97b103f52953605b14ceef96d94f64b3f888
FTyumin <feodor.tjumin28@gmail.com>
2023-06-21 16:23:01 +0300

    Login page

--- frontend/src/views/login.jsx
+++ frontend/src/views/login.jsx
@@ -71,14 +71,64 @@ const Login = () => {
         //     </Form>
         // </section>
         <Container size={420} my={40}>
-            <Title align="center" sx={(theme) =>({
-                fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-                fontWeight: 900,
-            })} 
-            >
-                Whatssup
+            <Title 
+                align="center"
+                sx={(theme) => ({
+                    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+                    fontWeight: 900,
+                })}
+                >Welcome
             </Title>
+
+            <Text color="dimmed" size="sm" align="center" mt={5}>
+                Do not have an account yet?{" "}
+                    <Anchor
+                    href="#"
+                    size="sm"
+                    onClick={(event) => event.preventDefault()}
+                    >
+                        Create account
+                    </Anchor>
+            </Text>
+                <Paper withBorder shadow="md" p={60} mt={30} radius="md">
+            <TextInput 
+            label="Username" 
+            placeholder="username" 
+            required 
+            id="username" 
+            name="username" 
+            value={username}
+            onChange={(e) => setUsername(e.target.value)} />
+
+            <PasswordInput
+            label="Password"
+            placeholder="Your password"
+            required
+            mt="md"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            />
+            <Group position="apart" mt="lg">
+            <Checkbox label="Remember me" sx={{ lineHeight: 1 }} />
+            <Anchor
+                onClick={(event) => event.preventDefault()}
+                href="#"
+                size="sm"
+            >
+                Forgot password?
+            </Anchor>
+            </Group>
+            <Button fullWidth mt="xl" type="submit">
+            Sign in
+            </Button>
+        </Paper>
+       
         </Container>
+       
+         
+          
 
      
     );
