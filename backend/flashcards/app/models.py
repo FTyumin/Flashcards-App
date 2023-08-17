2349b5e7bb49a17f9188f081464ea8b1ab7343d0
FTyumin <71492298+FTyumin@users.noreply.github.com>
2023-08-17 20:11:45 +0300

    Fixed adding of flashcards

--- backend/flashcards/app/models.py
+++ backend/flashcards/app/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Flashcard(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.CharField(max_length=255)
     answer = models.CharField(max_length=255)
 
